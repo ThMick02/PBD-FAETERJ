e55478367a5357fef6718cfc1811ca99785a8fb7
Thiago <thiagotavares0202@gmail.com>
2025-12-03 01:08:55 -0300

    Update crud.sql
    Bloco de Movimentações, 2 exemplos de locação para poder fazer outros comandos, agora que todas as tabelas estão preenchidas com algum item.

--- sql_fallscar/crud.sql
+++ sql_fallscar/crud.sql
@@ -1,8 +1,10 @@
 -- FUNCIONALIDADES DO SISTEMA:
 
+
 -- OBS:
--- COLOQUEI DESCRICAO EM TODAS AS FUNCIONALIDADES PARA FACILITAR COMPREENSÂSO.
+-- COLOQUEI DESCRICAO EM TODAS AS FUNCIONALIDADES PARA FACILITAR COMPREENSÃO.
 -- Vou separar em blocos de funcionalidades (Cadastros, Movimentações, Atualizações e Visualiação).
+
 
 -- BLOCO 1: Cadastros Básicos para preencher o Banco.
 
@@ -54,3 +56,39 @@ INSERT INTO seguro (Plano, Valor_Plano, Plano_Frota, Cobre_Furto, Cobre_Colisao,
 ('Seguro Frota', 300.00, 1, 1, 1, 1),
 ('Seguro Premium', 150.00, 0, 1, 1, 1),
 ('Seguro Econômico', 50.00, 0, 0, 1, 0);
+
+-- BLOCO 2: Movimentações de Locação, Pagamento e Pacote.
+
+-- CADASTRANDO LOCAÇÃO ÚNICA (Apenas um carro para um Cliente Pessoa)
+-- A locação com o Id do Argo Drive de Placa ABC1234, para o Thiago Tavares, atendido por Marcio Belo, com seguro Premium, no Galeão, para levar agora e devolver daqui 7 dias.
+-- Foi feito no Balcao e sem motorista contratado.
+INSERT INTO locacao (Id_Carro, Id_Cliente, Id_Funcionario, Id_Seguro, Id_Loja_Retirada, Data_Locacao, Data_Devolucao, Periodo, Valor_Locacao, motorista, Canal) VALUES 
+(1, 1, 1, 2, 1, NOW(), DATE_ADD(NOW(), INTERVAL 7 DAY), 7, 990.00, 0, 'Balcao');
+-- Criando o pagamento com o valor total e em uma única parcela.
+INSERT INTO pagamento (Valor_Total, Numero_Parcelas, Status) VALUES 
+(990.00, 1, 'Aprovado');
+-- O pacote que faz a associativa entre o pagamento e a locação, ele é utilizado em todas os pagamentos, no entanto, ele existe para caso o Cliente faça mais de uma locação (carro).
+INSERT INTO pacote (Id_Locacao, Id_Pagamento) VALUES 
+(1, 1);
+-- Por fim o carro assossiado a locação tem que receber um UPDATE, mudando seu Status para alugado. 
+UPDATE carro SET Status = 'Alugado', Id_Loja_Atual = NULL WHERE Id = 1;
+
+-- CADASTRANDO LOCAÇÃO ÚNICA (Frota para Cliente Empresa)
+-- Primeira locação (Segue os mesmos detalhes básicos que a do cliente), sendo que seleciona o Seguro Frota e pela Internet, ou seja, nãp recebe Id de Funcionário.
+INSERT INTO locacao (Id_Carro, Id_Cliente, Id_Funcionario, Id_Seguro, Id_Loja_Retirada, Data_Locacao, Data_Devolucao, Periodo, Valor_Locacao, Canal, Status) VALUES 
+(3, 2, NULL, 1, 2, NOW(), DATE_ADD(NOW(), INTERVAL 15 DAY), 15, 5500.00, 'Internet', 'Em Andamento');
+-- Fora isso, ambas as locações não tem motorista, pois geralmente a própria empresa teria os seus.
+INSERT INTO locacao (Id_Carro, Id_Cliente, Id_Funcionario, Id_Seguro, Id_Loja_Retirada, Data_Locacao, Data_Devolucao, Periodo, Valor_Locacao, Canal, Status) VALUES 
+(4, 2, NULL, 1, 3, NOW(), DATE_ADD(NOW(), INTERVAL 15 DAY), 15, 4500.00, 'Internet', 'Em Andamento');
+-- O pagamento realiazndo somando o valor total e aprovando o pagamento em 3 parcelas.
+INSERT INTO pagamento (Valor_Total, Numero_Parcelas, Status) VALUES 
+(10000.00, 3, 'Aprovado');
+-- Por fim, como dito anteriormente, o Id Pacote é responsável por associar as locações com o pagamento, ou seja, o Id das locações é alinhado com o do pagamento para adicionar o valor.
+INSERT INTO pacote (Id_Locacao, Id_Pagamento) VALUES 
+(2, 2),
+(3, 2);
+-- Novamente os carros locados recebem o status de "Alugado" e seu Id_Loja_Atual se torna nulo até que eles sejam devolvidos.
+UPDATE carro SET Status = 'Alugado', Id_Loja_Atual = NULL WHERE Id IN (3, 4);
+
+-- BLOCO 3: Atualiações do Banco de Dados.
+
